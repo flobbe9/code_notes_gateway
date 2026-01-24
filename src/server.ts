57f7b30b55dcceaf20216b82e86c42af3fcb8d98
flobbe9 <florin735@live.com>
2026-01-24 17:01:56 +0100

    refactor: listen to ssl file changes for ssl cert renewal to work

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import https from 'https';
 import app from './app.ts';
 import { BACKEND_BASE_URL, FROTNEND_BASE_URL, HTTPS, PORT, SSL_CRT_FILE, SSL_KEY_FILE, VERSION } from './helpers/constants.ts';
 import { logDebug, logInfo } from './helpers/logUtils.ts';
+import type { IncomingMessage, ServerResponse } from 'http';
 
 
 if (HTTPS) {
@@ -10,10 +11,34 @@ if (HTTPS) {
         cert: fs.readFileSync(SSL_CRT_FILE),
         key: fs.readFileSync(SSL_KEY_FILE)
     }
-    https.createServer(cts, app).listen(PORT, handleAppStart);
+    const server = https.createServer(cts, app);
+
+    // make sure that auto ssl renewal takes effect
+    fs.watchFile(SSL_CRT_FILE, () => {
+        handleSslChange(server);
+    });
+    fs.watchFile(SSL_KEY_FILE, () => {
+        handleSslChange(server);
+    });
+
+    server.listen(PORT, handleAppStart);
     
 } else {
     app.listen(PORT, handleAppStart);
+}
+
+/**
+ * Restart the server.
+ * 
+ * @param server `https.createServer`
+ */
+function handleSslChange(server: https.Server<typeof IncomingMessage, typeof ServerResponse>): void {
+    logDebug("Detected ssl certificate file changes, restarting server...")
+
+    server.close(() => {
+        server.listen(PORT, handleAppStart);
+        logDebug("Server restarted");
+    });
 }
 
 function handleAppStart(): void {
